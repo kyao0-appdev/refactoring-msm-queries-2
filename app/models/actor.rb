8c247ece863e112275d6bc3bd643addd33d66be0
kyao0 <kyao0@chicagobooth.edu>
2022-11-12 22:10:31 +0000

    100%

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,24 +11,26 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
-  def characters
-    key = self.id
+  has_many(:characters, { :class_name => "Character", :foreign_key =>"actor_id"})
+  # def characters
+  #   key = self.id
 
-    the_many = Character.where({ :actor_id => key })
+  #   the_many = Character.where({ :actor_id => key })
 
-    return the_many
-  end
+  #   return the_many
+  # end
 
-  def filmography
-    the_many = Array.new
+  has_many(:filmography, {:through => "characters", :source=>"movie"})
+  # def filmography
+  #   the_many = Array.new
 
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
+  #   self.characters.each do |joining_record|
+  #     destination_record = joining_record.movie
 
-      the_many.push(destination_record)
-    end
+  #     the_many.push(destination_record)
+  #   end
 
-    return the_many
-  end
+  #   return the_many
+  # end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,9 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  belongs_to(:director, { :class_name => "Director", :foreign_key => "director_id"})
+  has_many(:Characters, {:class_name =>"Characters", :foreign_key => "movie_id"})
   def director
     key = self.director_id
 
